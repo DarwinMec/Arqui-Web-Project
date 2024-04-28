732a7a6b9dea9677d9111fa52d9485110d2937c2
nacch <lennartrodriguez@hotmail.com>
2024-04-28 18:46:38 -0500

    Implementacion del Repositorio de la clase DetailOrder

---
+++ src/main/java/com/upc/tp_yapay/Repository/DetailOrderRepository.java
@@ -0,0 +1,7 @@
+package com.upc.tp_yapay.Repository;
+
+import com.upc.tp_yapay.Entities.DetailsOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DetailOrderRepository extends JpaRepository<DetailsOrder,Long> {
+}
